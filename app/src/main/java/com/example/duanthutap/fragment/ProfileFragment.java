a8f033aac39ad529aeea35837d3bdcdd59ed4665
tungdeptrai <tungktph27675@fpt.edu.vn>
2023-10-02 20:50:16 +0700

    Update Fragment Profile

--- app/src/main/java/com/example/duanthutap/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/duanthutap/fragment/ProfileFragment.java
@@ -7,14 +7,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.duanthutap.R;
 import com.example.duanthutap.activity.LoginActivity;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +32,10 @@ import com.google.firebase.auth.FirebaseAuth;
 public class ProfileFragment extends Fragment implements View.OnClickListener {
     private Button btnLogout;
     private FirebaseAuth firebaseAuth;
-
+    private FirebaseUser firebaseUser;
+    private TextView tvName;
+    private TextView tvEmail;
+    private DatabaseReference mReference;
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -48,15 +59,42 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        btnLogout = (Button) view.findViewById(R.id.btn_logout);
+        btnLogout = (Button) view.findViewById(R.id.btn_log_out);
+        tvName = (TextView) view.findViewById(R.id.tv_name);
+        tvEmail = (TextView) view.findViewById(R.id.tv_email);
+
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        mReference = FirebaseDatabase.getInstance().getReference();
+
+        setInfoProfile();
 
         btnLogout.setOnClickListener(this);
     }
 
+    private void setInfoProfile() {
+        String id = firebaseUser.getUid();
+        DatabaseReference userRef = mReference.child("user").child(id);
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name = snapshot.child("name").getValue(String.class);
+                String email = snapshot.child("email").getValue(String.class);
+                tvName.setText(name);
+                tvEmail.setText(email);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("Loi", "onCancelled: "+error.getMessage());
+            }
+        });
+    }
+
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.btn_logout){
+        if (view.getId() == R.id.btn_log_out){
             firebaseAuth.signOut();
             startActivity(new Intent(getActivity(), LoginActivity.class));
         }
